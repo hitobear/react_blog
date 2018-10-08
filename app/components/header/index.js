8571481aae856dcd602f6616afc9da3449442e84
hitobear <soliture@sina.com>
2018-10-09 06:29:12 +0800

    icon with size property

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import './index.less'
 import config from '../../../config/config'
 import {Link} from 'react-router-dom'
+import {Icon} from 'components'
 const apptitle=config.app.title
 export default class Header extends Component {
     
@@ -9,7 +10,8 @@ export default class Header extends Component {
         const avatar="http://blog-server.hunger-valley.com/avatar/48.jpg"
         return <header className='header'>
             <p className='title'><Link to="/" className='link'>{apptitle}</Link></p>
-            <div className='userset'>
+            <div className='flex-area'>
+                    <Link to="/" className='link'><Icon size="large" type="plus"/></Link>
                     <img src={avatar} className='avatar'></img>
             </div>
         </header>
--- app/components/icon/index.js
+++ app/components/icon/index.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import './index.less'
 const Icon=(props)=>{
-    const { type, className = '', spaceright,spaceleft,spin,...restProps } = props;
+  
+    const { type, className = '', size,spaceright,spaceleft,spin,...restProps } = props;
+    //size large,small
+    let sizeCls = '';
+    switch (size) {
+      case 'large':
+        sizeCls = 'lg';
+        break;
+      case 'small':
+        sizeCls = 'sm';
+      default:
+        break;
+    }
+    
     const classString = classNames('iconfont',{
+      [`lticon-${sizeCls}`]: sizeCls,
       lticon: true,
       'lticon-spin': !!spin || type === 'loading',
       [`icon-${type}`]: true,
       'space-right':spaceright?true:false,
       'space-left':spaceleft?true:false,
     }, className);
+    console.log(classString)
     return <span className={classString} {...restProps}/>;
 };
 
