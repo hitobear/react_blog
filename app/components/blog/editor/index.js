6d78d42afee0f4d4065aea52a234bbebf6b81e7a
hitobear <soliture@sina.com>
2018-12-09 09:33:56 +0800

    add scroll event

--- app/components/blog/editor/index.js
+++ app/components/blog/editor/index.js
@@ -5,6 +5,10 @@ import './index.less'
 
 
 class MarkDownEditor extends React.Component{
+    static defaultProps = {
+        content: ''
+    };
+
     state = this.initialState();
     initialState () {
         return {
@@ -19,7 +23,37 @@ class MarkDownEditor extends React.Component{
     componentWillReceiveProps(nextProps) {
         if (nextProps.content === this.props.content) { return; }
         this.setState({result: marked(nextProps.content)});
-      }
+    }
+    componentDidMount(){
+        console.log('this.preview')
+        console.log(this.preview)
+        this.previewControl = this.preview;
+        this._syncScroll = (() => {
+          let leftSync = false, rightSync = false;
+          let that = this;
+    
+          return function(e) {
+            let scrollEle = e.target;
+            let syncEle = scrollEle === that.textControl ? that.previewControl : that.textControl;
+            let percent = scrollEle.scrollTop / (scrollEle.scrollHeight - scrollEle.clientHeight);
+            if (leftSync && scrollEle === that.previewControl) {
+              return true;
+            }
+            if (rightSync && scrollEle === that.textControl) {
+              return true;
+            }
+    
+            leftSync = scrollEle === that.textControl;
+            rightSync = scrollEle === that.previewControl;
+            syncEle.scrollTop = percent * (syncEle.scrollHeight - syncEle.clientHeight);
+    
+            setTimeout(() => leftSync = rightSync = false, 100);
+          };
+        })();
+
+        (this.textControl ).addEventListener('scroll', e => this._syncScroll(e), false);
+        this.previewControl.addEventListener('scroll', this._syncScroll, false);
+    }
     
 
     render(){
@@ -27,9 +61,9 @@ class MarkDownEditor extends React.Component{
         <div className="editor">
             <div className="md_panel">
                 <div className="md_editor">
-                    <textarea onChange={this.props.onChange} value={this.props.content} />
+                    <textarea ref={div => this.textControl = div} onChange={this.props.onChange} value={this.props.content} />
                 </div>
-                <div className="md_preview"  dangerouslySetInnerHTML={{ __html: this.state.result }}>
+                <div className="md_preview"  ref={div => this.preview = div} dangerouslySetInnerHTML={{ __html: this.state.result }}>
                 </div>
             </div>
         </div>)
